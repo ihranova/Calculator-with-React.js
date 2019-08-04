768b4ca3059313bb6701e13505f559b9c756d2d7
ihranova <hranova.developer@gmail.com>
2019-08-04 22:10:08 +0300

    Update Calculator.js

--- src/Calculator.js
+++ src/Calculator.js
@@ -51,7 +51,7 @@ class Calculator extends Component {
 		            
 		        });
         	}else{
-			let {current} = this.state.current;
+			let {current} = this.state;
 			if(current.includes('.') && button === '.'){
 				this.setState({
 				    current: this.state.current,
